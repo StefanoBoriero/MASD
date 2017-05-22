7022a0d44c46361f64e727f1a6770625f1732864
Sławomir Fajer <fajer.slawek@gmail.com>
2017-05-23 01:02:34 +0200

    Add SQL connection and random car ordering

--- carFactory/src/java/carFactory/CarFactoryEnv.java
+++ carFactory/src/java/carFactory/CarFactoryEnv.java
@@ -3,30 +3,52 @@ package carFactory;
 import jason.environment.Environment;
 import jason.asSyntax.*;
 import jason.environment.*;
+
+import java.util.List;
+import java.util.Random;
 import java.util.logging.*;
 
 public class CarFactoryEnv extends Environment {
-	
-	  private Logger logger = Logger.getLogger("carFactory.mas2j."+CarFactoryEnv.class.getName());
+
+	private Logger logger = Logger.getLogger("carFactory.mas2j."+CarFactoryEnv.class.getName());
 
 
-	 @Override
-	  public void init(String[] args) {    }
+	@Override
+	public void init(String[] args) {    }
 
-	  @Override
-	  public boolean executeAction(String agName, Structure action) {
-	    if (action.getFunctor().equals("chooseCar")) {
-	      addPercept(Literal.parseLiteral("carOrdered(1)"));
-	      return true;
-	    } else {
-	      logger.info("executing: "+action+", but not implemented!");
-	      return false;
-	    }
-	  }
+	@Override
+	public boolean executeAction(String agName, Structure action) {		
+		if (action.getFunctor().equals("chooseCar")) {		
+			return chooseCar();
+		} else {
+			logger.info("executing: "+action+", but not implemented!");
+			return false;
+		}
+	}
 
-	  /** Called before the end of MAS execution */
-	  @Override
-	  public void stop() {
-	    super.stop();
-	  }
+	private boolean chooseCar() {
+		Random rand = new Random();
+		int i = 0;
+		DatabaseUtils db = new DatabaseUtils(logger);
+		List<Integer> cars = db.GetCars();
+		while(i < Constants.CAR_COUNT) {
+			int c = cars.get(rand.nextInt(cars.size()));
+			String literal = "carOrdered(" + c + ")";
+			logger.info("sending " + literal + " to salesman");
+			addPercept("salesman",Literal.parseLiteral(literal));	
+			i++;
+            try {
+				Thread.sleep(Constants.CAR_DELAY);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
+
+	/** Called before the end of MAS execution */
+	@Override
+	public void stop() {
+		super.stop();
+	}
 }
